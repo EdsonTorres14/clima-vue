1ecc5239b8b13d9a922eee17f8edbb40999869cc
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-07-12 20:42:42 -0600

    172. Primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -3,9 +3,14 @@ export default function useClima() {
     const obtenerClima = ({ ciudad, pais }) => {
         //Importar el Api key
         const key = import.meta.env.VITE_API_KEY
+
         //Obtenerla lat, lng
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+
+        console.log(url)
 
         //Obtener el clima
+
     }
 
 
