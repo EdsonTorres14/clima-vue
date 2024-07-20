b355b2578d1747140faa3ab28401b265e33f95df
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-07-19 19:13:02 -0600

    178. Mostrando un mensaje si no hay resultados

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 import Formulario from './components/Formulario.vue'
 import Clima from './components/Clima.vue';
 import Spiner from './components/Spiner.vue';
+import Alerta from './components/Alerta.vue';
 import useClima from './composables/useClima';
 
-const { obtenerClima, clima, cargando, mostrarClima } = useClima()
+const { obtenerClima, clima, cargando, mostrarClima, error } = useClima()
 
 </script>
 
@@ -14,6 +15,7 @@ const { obtenerClima, clima, cargando, mostrarClima } = useClima()
     <div class="contenedor buscador-clima">
         <Formulario @obtener-clima="obtenerClima" />
         <Spiner v-if="cargando" />
+        <Alerta v-if="error">{{ error }}</Alerta>
         <Clima v-if="mostrarClima" :clima="clima" />
     </div>
 </template>
